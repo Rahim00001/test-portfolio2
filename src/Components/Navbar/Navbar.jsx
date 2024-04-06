7e5b8c053406d50ed9c4d7411203a19913c790ae
Rahim00001 <rahimlpt2022@gmail.com>
2024-04-07 02:46:02 +0600

    navbar responsive done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import "./Navbar.css"
+import { FaAlignJustify } from "react-icons/fa6";
 const Navbar = () => {
     return (
         <>
             <nav className="nav-wrapper">
                 <div className="nav-content">
-                    <img className="logo" src="../../../public/Assets/images/logo.svg" alt="" />
+                    <img className="logo" src="/Assets/images/logo.svg" alt="" />
 
                     <ul>
                         <li><a className="menu-item" href="">Home</a></li>
@@ -13,6 +14,9 @@ const Navbar = () => {
                         <li><a className="menu-item" href="">Contact Me</a></li>
                         <button className="contact-btn" onClick={() => { }}>Hire Me</button>
                     </ul>
+                    <button className="menu-btn" onClick={() => { }}>
+                        <FaAlignJustify className="ml-[5px]" />
+                    </button>
                 </div>
             </nav>
         </>
