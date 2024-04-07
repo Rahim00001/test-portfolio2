3824dff1d7eaf5dfbf4172687c7d6406c34a6d91
Rahim00001 <rahimlpt2022@gmail.com>
2024-04-07 16:28:58 +0600

    mash gradient added on hero and animation added

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,12 +1,12 @@
 import "./Hero.css"
 const Hero = () => {
     return (
-        <section className="hero-container flex items-center relative">
-            <div className="hero-content flex-1">
-                <h2 className="text-6xl font-semibold leading-[5rem] mb-4">Building Digital Expriences That Inspire</h2>
-                <p className="w-[80%] leading-8">Passionate Frontend Developer | Transorming Ideas into Seamless and Visualy Stuning Web Solution</p>
+        <section className="hero-container">
+            <div className="hero-content">
+                <h2>Building Digital Expriences That Inspire</h2>
+                <p>Passionate Frontend Developer | Transorming Ideas into Seamless and Visualy Stuning Web Solution</p>
             </div>
-            <div className="hero-img flex-1 flex flex-col items-center gap-8 mt-20">
+            <div className="hero-img">
                 <div>
                     <div className="tech-icon">
                         <img src="../../../public/Assets/images/react.png" alt="" className="w-16 h-12" />
