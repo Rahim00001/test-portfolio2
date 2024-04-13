d365661c361e164d6e17c1dd308c20211aa85a41
Rahim00001 <rahimlpt2022@gmail.com>
2024-04-13 23:18:37 +0600

    image and icon added on hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,37 +1,26 @@
 import "./Hero.css"
+import hero from "../../../public/Assets/images/hero.png"
+import { IoLogoLinkedin } from "react-icons/io5";
+import { FaSquareGithub } from "react-icons/fa6";
+import { FaSquareFacebook } from "react-icons/fa6";
 const Hero = () => {
     return (
         // <section className="hero-container">
-        <section className="hero-container min-h-[80vh] flex flex-col items-center justify-center">
-            {/* <div className="hero-content">
-                <h2>Building Digital Expriences That Inspire</h2>
-                <p>Passionate Frontend Developer | Transorming Ideas into Seamless and Visualy Stuning Web Solution</p>
-            </div> */}
-            {/* <div className="hero-img">
-                <div>
-                    <div className="tech-icon">
-                        <img src="../../../public/Assets/images/react.png" alt="" className="w-16 h-12" />
-                    </div>
-                    <img src="../../../public/Assets/images/about_profile.svg" className="rounded-3xl" alt="" />
-                </div>
-                <div>
-                    <div className="tech-icon">
-                        <img src="../../../public/Assets/images/html.png" className="w-16 h-10" alt="" />
-                    </div>
-                    <div className="tech-icon">
-                        <img src="../../../public/Assets/images/css.png" className="w-16 h-10" alt="" />
-                    </div>
-                    <div className="tech-icon">
-                        <img src="../../../public/Assets/images/js.png" className="w-[50px] h-10" alt="" />
-                    </div>
-                </div>
-            </div> */}
+        <section className="hero-container min-h-[85vh] flex flex-col items-center justify-center">
             <div className="w-max">
+                <div className="flex justify-center items-center">
+                    <img src={hero} alt="" className="w-96" />
+                </div>
                 <p className="text-xl text-neutral-400">Hello! I am,</p>
                 <h1 className="text-7xl font-bold font-quicksand text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-indigo-600">Kamrul Islam Rahim</h1>
                 <h4 className="text-3xl text-neutral-300 text flex justify-end">Front-End Developer</h4>
             </div>
-            <p className="max-w-screen-lg mx-auto text-center mt-16">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae tenetur voluptate explicabo labore doloribus assumenda perspiciatis voluptatem incidunt suscipit sunt laudantium minima, voluptatibus reiciendis blanditiis error obcaecati, ad veniam? Fugit corrupti distinctio, expedita voluptas placeat ab laboriosam neque incidunt reprehenderit dolorum enim ipsum a voluptate est necessitatibus quis facere nostrum.</p>
+            <p className="max-w-[820px] mx-auto text-center mt-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti doloremque ex quos aspernatur cupiditate? Harum exercitationem expedita dolorum, fuga consequuntur quod placeat mollitia velit a rerum. Soluta culpa labore autem. Magnam maxime sed quam quae?</p>
+            <div className="text-neutral-400 text-3xl flex mt-5 gap-2">
+                <IoLogoLinkedin />
+                <FaSquareGithub />
+                <FaSquareFacebook />
+            </div>
         </section>
     );
 };
