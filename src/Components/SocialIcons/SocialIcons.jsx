61c949356f67128b3fa01dbf89fb6a3bed3cfb6d
Rahim00001 <rahimlpt2022@gmail.com>
2024-04-18 23:49:24 +0600

    social links added

--- src/Components/SocialIcons/SocialIcons.jsx
+++ src/Components/SocialIcons/SocialIcons.jsx
@@ -6,13 +6,13 @@ const SocialIcons = () => {
         <div className="hidden md:flex fixed flex-col top-[35%] left-0">
             <ul>
                 <li className="w-[150px] h-[50px] flex justify-between items-center px-4 ml-[-90px] hover:ml-[-10px] duration-300">
-                    <a className="flex justify-between items-center w-full text-gray-300" href="">LinedIn <FaLinkedin size={30} /></a>
+                    <a className="flex justify-between items-center w-full text-gray-300" href="https://www.linkedin.com/in/kamrul-islam-rahim">LinedIn <FaLinkedin size={30} /></a>
                 </li>
                 <li className="w-[150px] h-[50px] flex justify-between items-center px-4 ml-[-90px] hover:ml-[-10px] duration-300">
-                    <a className="flex justify-between items-center w-full text-gray-300" href="">Github <FaGithub size={30} /></a>
+                    <a className="flex justify-between items-center w-full text-gray-300" href="https://github.com/Rahim00001">Github <FaGithub size={30} /></a>
                 </li>
                 <li className="w-[150px] h-[50px] flex justify-between items-center px-4 ml-[-90px] hover:ml-[-10px] duration-300">
-                    <a className="flex justify-between items-center w-full text-gray-300" href="">Email <HiOutlineMail size={30} /></a>
+                    <a className="flex justify-between items-center w-full text-gray-300" href="rahimlpt2022@gmail.com">Email <HiOutlineMail size={30} /></a>
                 </li>
                 <li className="w-[150px] h-[50px] flex justify-between items-center px-4 ml-[-90px] hover:ml-[-10px] duration-300">
                     <a className="flex justify-between items-center w-full text-gray-300" href="">Resume <BsPersonLinesFill size={30} /></a>
