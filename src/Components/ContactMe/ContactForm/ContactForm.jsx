d4946a7b80a8ecb1337fed2971f4177bb54301fa
Rahim00001 <rahimlpt2022@gmail.com>
2024-04-22 18:03:24 +0600

    animation added on contact me section

--- src/Components/ContactMe/ContactForm/ContactForm.jsx
+++ src/Components/ContactMe/ContactForm/ContactForm.jsx
@@ -2,6 +2,8 @@ import "./ContactForm.css"
 const ContactForm = () => {
     return (
         <div className="contact-form-content">
+            <h2 className="text-[40px] text-[#d9dae7] font-semibold">Contact Me</h2>
+            <p className="text-neutral-300 mb-5">I will read all emails. Send me any message you want and I will get back to you.</p>
             <form action="">
                 <div className="name-container">
                     <input type="text" name="firstname" placeholder="First Name" />
--- src/Components/ContactMe/ContactMe.jsx
+++ src/Components/ContactMe/ContactMe.jsx
@@ -1,22 +1,24 @@
+import Lottie from "lottie-react";
 import ContactForm from "./ContactForm/ContactForm";
-import ContactInfoCard from "./ContactInfoCard/ContactInfoCard";
+import emailAnimation from "../../../public/Assets/emailSent.json"
+// import ContactInfoCard from "./ContactInfoCard/ContactInfoCard";
 import "./ContactMe.css"
 const ContactMe = () => {
     return (
         <section className="contact-container" id="contact">
-            <h5>Contact Me</h5>
-            <div className="cotact-content">
-                <div style={{ flex: 1 }}>
-                    <ContactInfoCard
+            <div className="cotact-content flex justify-center items-center">
+                <div className="max-w-[40%]">
+                    {/* <ContactInfoCard
                         iconUrl="../../../public/Assets/images/html.png"
                         text="rahimlpt2022@gmail.com"
                     ></ContactInfoCard>
                     <ContactInfoCard
                         iconUrl="../../../public/Assets/images/react.png"
                         text="https://github.com/Rahim00001"
-                    ></ContactInfoCard>
+                    ></ContactInfoCard> */}
+                    <Lottie animationData={emailAnimation} className=""></Lottie>
                 </div>
-                <div style={{ flex: 1 }}>
+                <div className="w-[50%]">
                     <ContactForm></ContactForm>
                 </div>
             </div>
