f04c97504629c63834ac1788c51f29ab23ad2bed
Rahim00001 <rahimlpt2022@gmail.com>
2024-04-23 23:54:10 +0600

    fabicon changed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,10 @@
 
 const Footer = () => {
     return (
-        <div className="mt-16">
+        <div className="mt-16" data-aos="fade-up" data-aos-duration="1000">
             <p className="font-sacramento text-neutral-300 text-5xl text-center max-w-screen-lg mx-auto">There are always some bugs in you code, in your life, also Everywhere. Just keep debugging.</p>
             <div className="flex justify-center">
-                <p className="mt-8 mb-1 text-sm font-thin">Copyright © 2024 KamrulIslamRahim | All rights reserved</p>
+                <p className="mt-8 text-sm font-thin">Copyright © 2024 KamrulIslamRahim | All rights reserved</p>
             </div>
         </div>
     );
